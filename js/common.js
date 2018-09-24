a5041d4f9a936e054dc2ef1084f98e0524b1680f
maureensayshi <maureen81711@gmail.com>
2018-09-24 16:39:58 +0800

    add open / close effect on menu buttons

---
+++ js/common.js
@@ -0,0 +1,9 @@
+document.querySelector(".btn_menu").onclick = function(){
+    document.querySelector(".menu").style.display = "flex";
+    document.querySelector(".btn_menu").style.visibility = "hidden";    
+};
+
+document.querySelector(".close-menu").onclick= function(){
+    document.querySelector(".menu").style.display = "none";
+    document.querySelector(".btn_menu").style.visibility = "";
+};
