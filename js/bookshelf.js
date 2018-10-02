c680d708134f8beac550e9aad30ef9b25970f975
maureensayshi <maureen81711@gmail.com>
2018-10-02 18:51:33 +0800

    show book on bookshelf page

--- js/bookshelf.js
+++ js/bookshelf.js
@@ -6,7 +6,6 @@ app.init = function () {
         app.uid = uid;
         app.showAll();
         app.menu();
-        app.closeLoading();
     });
 };
 
@@ -16,13 +15,27 @@ app.showAll = function () {
     let dbMember = db.ref("/members/" + app.uid + "/bookList/");
     dbMember.on("value", function (snapshot) {
         let val = snapshot.val();
-        console.log(val);
-        // let wrapper = app.get(".wrapper");
-        // for(let i = 0; i<val.length; i++){
+        if (val) {
+            console.log(val);
+            let bookArray = Object.values(val);
+            console.log(bookArray);
 
-        // }
 
+            let wrapper = app.get(".wrapper");
+            for (let i = 0; i < bookArray.length; i++) {
+                console.log(bookArray[i].key);
+                let parentDiv = app.createElement("div", "", "", "", "", wrapper);
+                let childDiv = app.createElement("div", "visual-book", "", "", "", parentDiv);
+                app.createElement("img", "", "", "src", bookArray[i].coverURL, childDiv);
+                app.createElement("div", "book-title", bookArray[i].title, "", "", parentDiv);
+            }
+            app.closeLoading();
+        } else {
+            console.log("no book");
+            app.closeLoading();
+        }
     });
+
 };
 
 window.addEventListener("DOMContentLoaded", app.init);
