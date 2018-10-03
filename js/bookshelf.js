a573bab6378eccfe90714fa690a05f022e3d0acd
maureensayshi <maureen81711@gmail.com>
2018-10-03 13:36:34 +0800

    refatory

--- js/bookshelf.js
+++ js/bookshelf.js
@@ -1,72 +1,54 @@
 "use strict";
 
 app.init = function () {
-    app.showLoading();
+    // app.showLoading();
     app.checkLogin().then(uid => {
         app.uid = uid;
-        app.showAll();
+        app.getStatus();
         app.menu();
-
     });
 };
 
-app.showAll = function () {
-    console.log(app.uid);
-    let db = app.database;
-    let dbMember = db.ref("/members/" + app.uid + "/bookList/");
-    dbMember.on("value", function (snapshot) {
-        let val = snapshot.val();
-        if (val) {
-            console.log(val);
-            let bookArray = Object.values(val);
-            let bookKey = Object.keys(val);
+app.getStatus = function () {
 
+    let readStatus = parseInt(window.location.search.split("?status=")[1]);
+    console.log(readStatus);
+    let bsTitle = app.get(".view>h2");
+    if (readStatus == "all") {
+        bsTitle.textContent = "總書櫃";
+    } else if (readStatus == 0) {
+        bsTitle.textContent = "未讀";
+    } else if (readStatus == 1) {
+        bsTitle.textContent = "閱讀中";
+    } else if (readStatus == 2) {
+        bsTitle.textContent = "已讀";
+    } else if (readStatus == "twice") {
+        bsTitle.textContent = "值得二讀";
+    } else if (readStatus == "lend") {
+        bsTitle.textContent = "出借的書";
+    }
+
+    app.allocateBS(readStatus);
+
+};
+
+app.allocateBS = function (readStatus) {
+    let dbMember = app.database.ref("/members/" + app.uid + "/bookList/");
+    dbMember.orderByChild("readStatus").equalTo(readStatus).once("value").then((snapshot) => {
+        console.log(snapshot.val());
+        let bookSelected = Object.values(snapshot.val());
+        let bookKey = Object.keys(snapshot.val());
+        for (let i = 0; i < bookSelected.length; i++) {
             let wrapper = app.get(".wrapper");
-
-            for (let i = 0; i < bookArray.length; i++) {
-                if (window.location.search == "?status=0") {
-                    if (bookArray[i].readStatus == 0) {
-                        app.createBook(wrapper, bookArray, bookKey, i);
-                        app.closeLoading();
-                    }
-                } else if (window.location.search == "?status=1") {
-                    if (bookArray[i].readStatus == 1) {
-                        app.createBook(wrapper, bookArray, bookKey, i);
-                        app.closeLoading();
-                    }
-                    app.closeLoading();
-                } else if (window.location.search == "?status=2") {
-                    if (bookArray[i].readStatus == 2) {
-                        app.createBook(wrapper, bookArray, bookKey, i);
-                        app.closeLoading();
-                    }
-                } else if (window.location.search == "?status=all") {
-                    if (bookArray[i].readStatus) {
-                        app.createBook(wrapper, bookArray, bookKey, i);
-                        app.closeLoading();
-                    }
-                } else if (window.location.search == "?status=twice") {
-                    if (bookArray[i].twice) {
-                        app.createBook(wrapper, bookArray, bookKey, i);
-                        app.closeLoading();
-                    }
-                }
-            }
-
-        } else {
-            console.log("no book");
+            let parentDiv = app.createElement("div", "", "", "", "", wrapper);
+            let nav = app.createElement("a", "", "", "href", "book.html?id=" + bookKey[i], parentDiv);
+            let childDiv = app.createElement("div", "visual-book", "", "", "", nav);
+            app.createElement("img", "", "", "src", bookSelected[i].coverURL, childDiv);
+            app.createElement("div", "book-title", bookSelected[i].title, "", "", nav);
             app.closeLoading();
         }
     });
-
 };
 
-app.createBook = function (wrapper, bookArray, bookKey, i) {
-    let parentDiv = app.createElement("div", "", "", "", "", wrapper);
-    let nav = app.createElement("a", "", "", "href", "book.html?id=" + bookKey[i], parentDiv);
-    let childDiv = app.createElement("div", "visual-book", "", "", "", nav);
-    app.createElement("img", "", "", "src", bookArray[i].coverURL, childDiv);
-    app.createElement("div", "book-title", bookArray[i].title, "", "", nav);
-};
 
 window.addEventListener("DOMContentLoaded", app.init);
