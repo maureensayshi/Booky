c5b629de9c0aafec2fbd4332c13233e4fec91fe8
maureensayshi <maureen81711@gmail.com>
2018-10-18 23:24:11 +0800

    move add to google calendar feature to book.js

--- booky/js/book.js
+++ booky/js/book.js
@@ -9,114 +9,108 @@ app.init = function () {
         app.searchBar();
         app.addBookInit();
         app.scanBookInit();
-        // Client ID and API key from the Developer Console
-        app.clientId = "757419169220-9ehr4saki2pbqpp4c2imqa3qd8nbuf0q.apps.googleusercontent.com";
-        app.apiKey = "AIzaSyALgpVirl6lyBvOK9W--e5QycFeMFzcPLg";
 
-        // Array of API discovery doc URLs for APIs used by the quickstart
-        app.discoveryDoc = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-
-        // Authorization scopes required by the API; multiple scopes can be
-        // included, separated by spaces.
-        app.scopes = "https://www.googleapis.com/auth/calendar.events";
-        // app.googleCal();
     });
 };
 
-app.googleCal = function () {
+app.handleClientLoad = function () {
+    gapi.load("client:auth2", app.initClient);
+}
 
+app.initClient = function () {
+    // Client ID and API key from the Developer Console
+    app.clientId = "757419169220-9ehr4saki2pbqpp4c2imqa3qd8nbuf0q.apps.googleusercontent.com";
+    app.apiKey = "AIzaSyALgpVirl6lyBvOK9W--e5QycFeMFzcPLg";
 
-    function handleClientLoad() {
-        gapi.load("client:auth2", initClient);
-    };
+    // Array of API discovery doc URLs for APIs used by the quickstart
+    app.discoveryDoc = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
 
-    handleClientLoad();
+    // Authorization scopes required by the API; multiple scopes can be
+    // included, separated by spaces.
+    app.scopes = "https://www.googleapis.com/auth/calendar.events";
+    // app.handleClientLoad();
+    console.log(app.clientId);
+    console.log(app.apiKey);
+    console.log(app.discoveryDoc);
+    console.log(app.scopes);
 
-    function initClient() {
-        gapi.client.init({
-            apiKey: app.apiKey,
-            clientId: app.clientId,
-            discoveryDocs: app.discoveryDoc,
-            scope: app.scopes
-        }).then(function () {
-            // Listen for sign-in state changes.
-            gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+    gapi.client.init({
+        apiKey: app.apiKey,
+        clientId: app.clientId,
+        discoveryDocs: app.discoveryDoc,
+        scope: app.scopes
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-            // Handle the initial sign-in state.
-            updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-            authorizeButton.onclick = handleAuthClick;
-            signoutButton.onclick = handleSignoutClick;
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        app.get("#authorize_button").onclick = handleAuthClick;
+        app.get("#signout_button").onclick = handleSignoutClick;
+    });
+
+    /**
+  *  Sign in the user upon button click.
+  */
+    function handleAuthClick(event) {
+        gapi.auth2.getAuthInstance().signIn();
+    }
+
+    /**
+     *  Sign out the user upon button click.
+     */
+    function handleSignoutClick(event) {
+        gapi.auth2.getAuthInstance().signOut();
+    }
+
+    app.get("#calendar").onclick = insertEvent;
+
+    function insertEvent() {
+        var event = {
+            "summary": "Google I/O 2015",
+            "location": "800 Howard St., San Francisco, CA 94103",
+            "description": "A chance to hear more about Google\"s developer products.",
+            "start": {
+                "dateTime": "2018-10-28T09:00:00-07:00",
+                "timeZone": "America/Los_Angeles"
+            },
+            "end": {
+                "dateTime": "2018-10-29T17:00:00-07:00",
+                "timeZone": "America/Los_Angeles"
+            },
+            "recurrence": [
+                "RRULE:FREQ=DAILY;COUNT=2"
+            ],
+            "reminders": {
+                "useDefault": false,
+                "overrides": [
+                    { "method": "email", "minutes": 24 * 60 },
+                    { "method": "popup", "minutes": 10 }
+                ]
+            }
+        };
+
+        var request = gapi.client.calendar.events.insert({
+            "calendarId": "primary",
+            "resource": event
         });
 
-        /**
-      *  Sign in the user upon button click.
-      */
-        function handleAuthClick(event) {
-            gapi.auth2.getAuthInstance().signIn();
-        }
-
-        /**
-         *  Sign out the user upon button click.
-         */
-        function handleSignoutClick(event) {
-            gapi.auth2.getAuthInstance().signOut();
-        }
-
-        function insertEvent() {
-            var event = {
-                'summary': 'Google I/O 2015',
-                'location': '800 Howard St., San Francisco, CA 94103',
-                'description': 'A chance to hear more about Google\'s developer products.',
-                'start': {
-                    'dateTime': '2018-10-28T09:00:00-07:00',
-                    'timeZone': 'America/Los_Angeles'
-                },
-                'end': {
-                    'dateTime': '2018-10-29T17:00:00-07:00',
-                    'timeZone': 'America/Los_Angeles'
-                },
-                'recurrence': [
-                    'RRULE:FREQ=DAILY;COUNT=2'
-                ],
-                'reminders': {
-                    'useDefault': false,
-                    'overrides': [
-                        { 'method': 'email', 'minutes': 24 * 60 },
-                        { 'method': 'popup', 'minutes': 10 }
-                    ]
-                }
-            };
-
-            var request = gapi.client.calendar.events.insert({
-                'calendarId': 'primary',
-                'resource': event
-            });
-
-            request.execute(function (event) {
-                appendPre('Event created: ' + event.htmlLink);
-            });
-        }
-
+        request.execute(function (event) {
+            app.get("#calendarLink").textContent = event.htmlLink;
+            // appendPre("Event created: " + event.htmlLink);
+        });
     }
 
     function updateSigninStatus(isSignedIn) {
         if (isSignedIn) {
-            authorizeButton.style.display = 'none';
-            signoutButton.style.display = 'block';
-            listUpcomingEvents();
+            app.get("#authorize_button").style.display = "none";
+            app.get("#signout_button").style.display = "block";
         } else {
-            authorizeButton.style.display = 'block';
-            signoutButton.style.display = 'none';
+            app.get("#authorize_button").style.display = "block";
+            app.get("#signout_button").style.display = "none";
         }
     }
-
-
 }
-
-
-
-
-
 
 
 app.showBook = function () {
